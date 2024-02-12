6b5c82e8e50d457097ffb7c6dd197b88d7b79f43
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-02-12 18:08:03 -0300

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,1 +1,49 @@
+const Liquid = require('liquidjs-lib');
 
+// Initialize Liquid network
+const network = Liquid.Networks.liquid;
+
+// Create new transaction builder
+const txb = new Liquid.TransactionBuilder(network);
+
+// Define inputs
+const input1 = {
+    txid: 'input1_txid',
+    vout: input1_vout,
+    scriptPubKey: input1_scriptPubKey,
+    value: input1_value,
+};
+const input2 = {
+    txid: 'input2_txid',
+    vout: input2_vout,
+    scriptPubKey: input2_scriptPubKey,
+    value: input2_value,
+};
+// Add inputs to the transaction builder
+txb.addInput(input1);
+txb.addInput(input2);
+
+// Define outputs
+const output1 = {
+    script: Liquid.address.toOutputScript('output1_address', network),
+    value: output1_value,
+};
+const output2 = {
+    script: Liquid.address.toOutputScript('output2_address', network),
+    value: output2_value,
+};
+// Add outputs to the transaction builder
+txb.addOutput(output1);
+txb.addOutput(output2);
+
+// Sign inputs
+// NOTE: Signing inputs would require private keys
+// For CoinJoin, this would be handled by participants
+// You would need to implement the logic for participants to sign their inputs
+
+// Finalize the transaction
+txb.setVersion(2); // Optional: Set transaction version
+const tx = txb.buildIncomplete();
+
+// Print out the transaction hex
+console.log(tx.toHex());
